4020cd7a6bee0afad89251ac52191ea7246eb608
Taragohf <ajos.chungos@gmail.com>
2016-12-02 12:25:23 +0100

    Report finished

--- report/src/a24/main.cc
+++ report/src/a24/main.cc
@@ -19,15 +19,17 @@ int main()
 
     std::cout << std::endl;
     //End algorithm from exercise 22
-    for (std::size_t position = 0, posCompare = position;
-        position <= vec.size(); ++position)
+    for (std::size_t position = 0, posCompare
+        = position; position <= vec.size();
+        ++position)
     {
         if (vec[posCompare] != vec[position])
         {
-            std::size_t times = position - posCompare - 1;
-            std::cout << "The element " << vec[posCompare]
-                << " is repited " << times << " times"
-                << std::endl;
+            std::size_t times = position -
+                posCompare - 1;
+            std::cout << "The element "
+                << vec[posCompare] << " is repited "
+                << times << " times" << std::endl;
             posCompare = position;
         }
     }
